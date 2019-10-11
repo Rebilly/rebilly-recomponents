fa90388dd964acf15078b921d2ee899c36fce8b0
Junyong <zengjunyong@gmail.com>
2019-10-11 15:45:02 +0800

    test(r-date-input): add all the props

--- packages/recomponents/src/components/r-date-input/r-date-input.story.js
+++ packages/recomponents/src/components/r-date-input/r-date-input.story.js
@@ -1,12 +1,40 @@
 import {storiesOf} from '@storybook/vue';
 import {action} from '@storybook/addon-actions';
-import {select} from '@storybook/addon-knobs';
+import {boolean, select, text} from '@storybook/addon-knobs';
 import RDateInput from './r-date-input.vue';
 import markdown from './r-date-input.md';
 
 storiesOf('Components/DateInput', module)
     .add('Date', () => ({
         props: {
+            availableDates: {
+                default: select(
+                    'availableDates',
+                    {
+                        null: null,
+                        'start from today': {
+                            start: new Date(),
+                            end: null,
+                        },
+                    },
+                    null,
+                ),
+            },
+            label: {
+                default: text('label', ''),
+            },
+            disabled: {
+                default: boolean('disabled', false),
+            },
+            placeholder: {
+                default: text('placeholder', ''),
+            },
+            helpText: {
+                default: text('helpText', ''),
+            },
+            stack: {
+                default: text('stack', ''),
+            },
             type: {
                 default: select(
                     'type',
@@ -18,6 +46,12 @@ storiesOf('Components/DateInput', module)
                     'date',
                 ),
             },
+            validate: {
+                default: select('validate', [null], null),
+            },
+            noFlex: {
+                default: boolean('noFlex', false),
+            },
         },
         data: () => ({
             date: null,
@@ -26,7 +60,18 @@ storiesOf('Components/DateInput', module)
             input: action('input'),
         },
         components: {RDateInput},
-        template: `<r-date-input :type="type"  v-model="date" @input="input"/>`,
+        template: `<r-date-input 
+                        :available-dates="availableDates"
+                        :label="label"
+                        :disabled="disabled"
+                        :placeholder="placeholder"
+                        :helpText="helpText"
+                        :stack="stack"
+                        :type="type"  
+                        :validate="validate"  
+                        :noFlex="noFlex"  
+                        v-model="date"
+                        @input="input"/>`,
     }), {
         notes: {markdown},
     });
