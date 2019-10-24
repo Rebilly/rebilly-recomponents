6de2e580eaeb1f712c1ed97f5d666ba3171a34ad
Junyong <zengjunyong@gmail.com>
2019-10-24 10:46:52 +0800

    test(r-button-group): add tests with 100% coverage

---
+++ packages/recomponents/src/components/r-button-group/r-button-group.spec.js
@@ -0,0 +1,15 @@
+import {mount} from '@vue/test-utils';
+import RButtonGrup from './r-button-group.vue';
+
+describe('r-button-group.vue', () => {
+    it('should render component with fluid prop', () => {
+        const wrapper = mount(RButtonGrup, {
+            propsData: {
+                fluid: true,
+            },
+        });
+
+        expect(wrapper.classes())
+            .toContain('r-button-group-fluid');
+    });
+});
