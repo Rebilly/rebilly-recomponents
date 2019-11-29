57950511bbc659454497374877ac2c9bf4a378b0
Vladislav Pozniak <izvergsklizmoy@gmail.com>
2019-11-29 19:50:26 +0300

    feat(component): Add image support

--- packages/recomponents/src/components/r-avatar/r-avatar.story.js
+++ packages/recomponents/src/components/r-avatar/r-avatar.story.js
@@ -2,18 +2,37 @@ import {storiesOf} from '@storybook/vue';
 import {text, select} from '@storybook/addon-knobs';
 import markdown from './r-avatar.md';
 
+import {icons} from '../../../.storybook/knobs';
+
 storiesOf('Components', module)
     .add('Avatar', () => ({
         template: `
             <r-avatar
                 :initials="initials"
-                :color="color">
-                kek
+                :description="desciptionProp"
+                :image="image"
+                :color="color"
+                :icon="icon">
+                <template v-if="descriptionSlot">
+                    <div v-html="descriptionSlot"></div>
+                </template>
             </r-avatar>
         `,
         props: {
             initials: {
-                default: text('Initial', 'VP'),
+                default: text('Initials', 'VP', 'Text'),
+            },
+            descriptionSlot: {
+                default: text('Description as slot', `<p>Some Text</p>\n<p class="avatar-info-muted-text">Some Text</p>`, 'Text'),
+            },
+            desciptionProp: {
+                default: text('Description as prop', '', 'Text'),
+            },
+            image: {
+                default: text('Image', 'https://www.meme-arsenal.com/memes/86b1de6ff6981be012d40bf26c6992ab.jpg'),
+            },
+            icon: {
+                default: select('Icon', icons, null),
             },
             color: {
                 default: select('Color', [
@@ -30,4 +49,7 @@ storiesOf('Components', module)
         },
     }), {
         notes: {markdown},
+        knobs: {
+            escapeHTML: false,
+        },
     });
--- packages/recomponents/src/components/r-avatar/r-avatar.vue
+++ packages/recomponents/src/components/r-avatar/r-avatar.vue
@@ -1,13 +1,21 @@
 <template>
     <div class="avatar avatar-description">
         <div class="avatar-image" v-fs-block>
-            <div class="avatar-initials" :class="['avatar-initials-' + initialsColor]">
+            <div class="avatar-initials" :class="['avatar-initials-' + initialsColor]" v-if="initials">
                 <span>{{initials}}</span>
+            </div>
+            <div class="avatar-icon" v-if="icon">
+                <r-icon :icon="icon"/>
+            </div>
+            <div class="avatar-image-image">
+                <r-img :src="image"/>
             </div>
         </div>
         <div class="avatar-info">
-            <p>Some Text</p>
-            <p class="avatar-info-muted-text">Some Text</p>
+            <template v-if="description">
+                {{description}}
+            </template>
+            <slot></slot>
         </div>
     </div>
 </template>
@@ -19,8 +27,23 @@
         props: {
             initials: {
                 type: String,
+                validator: function (value) {
+                    if (value.length > 2) {
+                        console.warn('RAvatar: should provide only 2 symbols for initials')
+                    }
+                    return true
+                },
+            },
+            description: {
+                type: String,
+            },
+            image: {
+                type: String,
             },
             color: {
+                type: String,
+            },
+            icon: {
                 type: String,
             }
         },
