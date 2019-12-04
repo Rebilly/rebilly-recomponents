461da393e1d0658f8d134f16490cdfd62e72b9f1
Mathew Sonke <mathew.sonke@rebilly.com>
2019-12-04 12:22:54 -0500

    Move components and directives to their own index file

---
+++ packages/recomponents/src/directives/index.js
@@ -0,0 +1,13 @@
+import RTooltip from './r-tooltip/r-tooltip';
+import RFsBlock from './r-fs-block';
+import RLazy from './r-lazy';
+import RContent from './r-content/r-content';
+import RClickOutside from './r-click-outside/r-click-outside';
+
+export {
+    RTooltip,
+    RFsBlock,
+    RLazy,
+    RContent,
+    RClickOutside,
+};
