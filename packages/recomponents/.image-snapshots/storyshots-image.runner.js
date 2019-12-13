df9c30381e15aa9b2ec6ad88b6dd8cd40c828f9c
eduard.titov <editied@gmail.com>
2019-12-14 01:20:46 +0300

    test: setup storybook-storyshots imane-snapshots for storybook demos
    Setup new tests based on image-snapshots with addon-storyshots-puppeteer, Add new test command
    test:image-snapshots, Split test command into 2 commands test:unit and test:image-snapshots

--- packages/recomponents/.image-snapshots/storyshots-image.runner.js
+++ packages/recomponents/.image-snapshots/storyshots-image.runner.js
@@ -1,20 +1,69 @@
+/**
+ *
+ * Image snapshots with storybook + jest + puppeteer
+ * https://github.com/storybookjs/storybook/tree/master/addons/storyshots/storyshots-puppeteer
+ *
+ */
 const path = require('path');
 const fs = require('fs');
+const pkg = require('../package');
 
+import initStoryshots from '@storybook/addon-storyshots';
+import {imageSnapshot} from '@storybook/addon-storyshots-puppeteer';
+
+// storybook output directory
 const pathToStorybookStatic = path.join(__dirname, '../../../docs');
+const storybookUrl = `file://${pathToStorybookStatic}`;
 
-const initStoryshots = require('@storybook/addon-storyshots').default;
-const {imageSnapshot} = require('@storybook/addon-storyshots-puppeteer');
+// we should wait for data loading in that components
+const componentsWithAsyncData = [
+    // TODO
+    ['components-select--async-data', 2000],
+];
+
+// the loader animation made some noise
+const componentsWithLoaderIcon = [
+    'components-button--state',
+    'components-loader--component',
+    'components-loader--example',
+    'components-select--async-data',
+];
+
+const getGotoOptions = () => ({waitUntil: 'networkidle0'});
+
+const beforeScreenshot = (page, {url}) => new Promise((resolve) => {
+
+    const [name, delay] = componentsWithAsyncData.find(([name]) => url.endsWith(`iframe.html?id=${name}`)) || [];
+
+    if (!name) {
+        resolve();
+    } else {
+        setTimeout(resolve, delay);
+    }
+});
+
+const getMatchOptions = ({url}) => {
+    if (componentsWithLoaderIcon.find(name => url.endsWith(`iframe.html?id=${name}`))) {
+        return {
+            failureThreshold: 0.015,
+            failureThresholdType: 'percent',
+        };
+    }
+    return {};
+};
 
 if (!fs.existsSync(pathToStorybookStatic)) {
     console.log('You are running image snapshots without having the static build of storybook. Please run "yarn run build-storybook" before running tests. path:');
     process.exit(1);
 } else {
     initStoryshots({
-        suite: 'Recomponents snapshots',
-        configPath: path.join(__dirname, './'),
+        suite: `${pkg.name} ${pkg.version}`,
+        configPath: path.join(__dirname, '../.storybook/config.js'),
         test: imageSnapshot({
-            storybookUrl: `file://${pathToStorybookStatic}`,
+            storybookUrl,
+            getGotoOptions,
+            getMatchOptions,
+            beforeScreenshot,
         }),
     });
 }
--- packages/recomponents/babel.config.js
+++ packages/recomponents/babel.config.js
@@ -2,4 +2,19 @@ module.exports = {
     presets: [
         '@vue/app',
     ],
+    env: {
+        test: {
+            presets: [
+                [
+                    '@babel/preset-env',
+                    {
+                        targets: {
+                            node: 'current',
+                        },
+                    },
+                ],
+            ],
+            plugins: ['babel-plugin-require-context-hook'],
+        },
+    },
 };
