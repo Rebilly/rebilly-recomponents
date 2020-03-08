fb089fed8f7341d5ee7bd401cbbcae2bf872ae87
Vladislav Pozniak <izvergsklizmoy@gmail.com>
2020-03-08 15:53:58 +0300

    chore(test): Fix RSelect test

--- packages/recomponents/src/components/r-select/r-select.spec.js
+++ packages/recomponents/src/components/r-select/r-select.spec.js
@@ -1,3 +1,4 @@
+import Vue from 'vue';
 import {shallowMount} from '@vue/test-utils';
 import {renderToString} from '@vue/server-test-utils';
 import RSelect from './r-select.vue';
@@ -257,6 +258,9 @@ describe('r-select.vue', () => {
             },
         });
         wrapper.setData({search: 'test'});
+
+        await Vue.nextTick();
+
         expect(wrapper.emitted()['search-change']).toEqual([['test', 'id']]);
     });
     it('should set isOpen value to true', () => {
