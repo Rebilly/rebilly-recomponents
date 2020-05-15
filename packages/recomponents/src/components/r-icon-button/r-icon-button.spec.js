1592809b478a1d3eb2cb662b0d3562b77b472c4d
Junyong <zengjunyong@gmail.com>
2020-05-15 18:19:32 +0800

    fix(r-button): add unit test for r-icon-button

--- packages/recomponents/src/components/r-icon-button/r-icon-button.spec.js
+++ packages/recomponents/src/components/r-icon-button/r-icon-button.spec.js
@@ -1,4 +1,5 @@
 import {renderToString} from '@vue/server-test-utils';
+import {shallowMount} from '@vue/test-utils';
 import RIconButton from './r-icon-button.vue';
 
 describe('r-icon-button.vue', () => {
@@ -6,5 +7,15 @@ describe('r-icon-button.vue', () => {
         const wrapper = await renderToString(RIconButton, {});
 
         expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render component with capitalizing the first letter', async () => {
+        const wrapper = shallowMount(RIconButton, {
+            slots: {
+                default: 'click me',
+            },
+        });
+
+        expect(wrapper.text()).toMatch('Click me');
     });
 });
