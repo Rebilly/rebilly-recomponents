bf4e478c08c85e0093e95fdc580ff3df3a9827d1
Junyong <zengjunyong@gmail.com>
2020-05-15 17:33:24 +0800

    fix(r-button): solve the undefined exception in unit tests

--- packages/recomponents/src/components/mixins/capitalize-the-first-letter-in-slot-mixin.js
+++ packages/recomponents/src/components/mixins/capitalize-the-first-letter-in-slot-mixin.js
@@ -1,11 +1,13 @@
 export default {
     methods: {
         capitalizeTheFirstLetterInSlot() {
-            let {text} = this.$slots.default[0];
-            if (text) {
-                text = text.trim();
-                text = text.charAt(0).toUpperCase() + text.slice(1);
-                this.$slots.default[0].text = text;
+            if (this.$slots.default) {
+                let {text} = this.$slots.default[0];
+                if (text) {
+                    text = text.trim();
+                    text = text.charAt(0).toUpperCase() + text.slice(1);
+                    this.$slots.default[0].text = text;
+                }
             }
         },
     },
