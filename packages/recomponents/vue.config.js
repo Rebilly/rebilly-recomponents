353e387733123f51d9ca126343c181576052d3c3
John Hannagan <weetbix_@hotmail.com>
2022-08-25 11:19:09 +0800

    Minimize bundle size

--- packages/recomponents/vue.config.js
+++ packages/recomponents/vue.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
 module.exports = {
     outputDir: 'dist',
@@ -9,8 +10,20 @@ module.exports = {
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, 'src'),
+                // Deduplicate any moment imports, otherwise we end up with
+                // it included in the bundle twice, even though we only have
+                // 1 version of moment.
+                moment: path.resolve(__dirname, 'node_modules/moment'),
             },
         },
+        plugins: [
+            // By default moment bundles all the locales with it, we do not
+            // support different locales for any of the projects that use recomponents
+            // so we can remove these huge files
+            new MomentLocalesPlugin({
+                localesToKeep: ['es-us'],
+            }),
+        ],
     },
     css: {
         extract: true,
